95276d4f0fcac9e5d1013d90d772f68d43d8c8af
Bharat Naganath <bharatsn123@gmail.com>
2019-06-22 12:49:38 +0530

    Add files via upload

---
+++ kube.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then
+	echo "1. Create Server and Services \n2. Delete Server and Services\n"
+	read op
+else
+	op=$1
+fi
+if [ $op -eq 1 ]
+then
+	echo "INFO : Creating server---"
+	kubectl create -f teastore-rabbitmq.yaml
+	echo "INFO : Sleeping for 3 seconds... Giving time for server to come online.. Please Wait"
+	sleep 3s
+	echo "INFO : Creating services---"
+	kubectl create -f teastore-ribbon-kieker.yaml
+else
+	echo "INFO : Deleting server---"
+	kubectl delete -f teastore-rabbitmq.yaml
+	echo "INFO : Deleting services---"
+	kubectl delete -f teastore-ribbon-kieker.yaml
+fi
+
+echo "INFO : build.sh is done "
