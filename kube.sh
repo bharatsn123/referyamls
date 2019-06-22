12ec916d669e19623fd16ed73a174e0057bcee7d
Bharat Naganath <bharatsn123@gmail.com>
2019-06-22 12:51:31 +0530

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -10,16 +10,16 @@ fi
 if [ $op -eq 1 ]
 then
 	echo "INFO : Creating server---"
-	kubectl create -f teastore-rabbitmq.yaml
+	kubectl create -f https://raw.githubusercontent.com/bharatsn123/referyamls/master/teastore-rabbitmq.yaml
 	echo "INFO : Sleeping for 3 seconds... Giving time for server to come online.. Please Wait"
 	sleep 3s
 	echo "INFO : Creating services---"
-	kubectl create -f teastore-ribbon-kieker.yaml
+	kubectl create -f https://raw.githubusercontent.com/bharatsn123/referyamls/master/teastore-ribbon-kieker.yaml
 else
 	echo "INFO : Deleting server---"
-	kubectl delete -f teastore-rabbitmq.yaml
+	kubectl delete -f https://raw.githubusercontent.com/bharatsn123/referyamls/master/teastore-rabbitmq.yaml
 	echo "INFO : Deleting services---"
-	kubectl delete -f teastore-ribbon-kieker.yaml
+	kubectl delete -f https://raw.githubusercontent.com/bharatsn123/referyamls/master/teastore-ribbon-kieker.yaml
 fi
 
 echo "INFO : build.sh is done "
